97e4c57a3eaac447356d6e35e6767d7d7db6061a
Sara Mahran <saramahran20@gmail.com>
2022-07-20 20:54:13 +0200

    Commit

---
+++ main.js
@@ -0,0 +1,73 @@
+function computerPlay() {
+    let computerChoice = Math.floor(Math.random() * 3 + 1);
+    let computerSelection = computerChoice === 1 ? "rock" : computerChoice === 2 ? "paper" : "scissors";
+    console.log(computerSelection);
+    return computerSelection;
+}
+
+function playerPlay() {
+    const playerInput = prompt("Please enter rock, or paper, or scissors to start the game.");
+    const playerSelection = playerInput.trim().toLowerCase();
+
+    if (playerSelection != "rock" || "paper" || "scissors") {
+        alert("Wrong input, please enter rock, paper, or scissors");
+    }
+
+    return playerSelection;
+}
+
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection == computerSelection) {
+
+        return "It's a tie";
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "Computer wins!";
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "Computer wins!";
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "Computer wins!";
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You win!";
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You win!";
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You win!";
+    }
+}
+
+let playerScore = 0;
+let computerScore = 0;
+
+function game() {
+
+    for (let round = 0; round < 5; round++) {
+        const playerSelection = playerPlay();
+        const computerSelection = computerPlay();
+        let gameRound = playRound(playerSelection, computerSelection);
+        console.log(gameRound);
+        console.log("-----------")
+        if (gameRound == "Computer wins!") {
+            computerScore++;
+        } else if (gameRound == "You win!") {
+            playerScore++;
+        }
+    }
+
+    if (computerScore > playerScore) {
+        console.log("Final result: COMPUTER WINS!!");
+    } else if (playerScore > computerScore) {
+        console.log("Final result: PLAYER WINS!!");
+    } else {
+        console.log("Final result: IT'S A TIE");
+    }
+
+
+    return computerScore;
+    return playerScore;
+
+}
+
+game();
+let playAgain = confirm("Play again?");
+playAgain ? location.reload() : alert("Okay, see you again!");
